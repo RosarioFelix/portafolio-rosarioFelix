8ba13ff451b713eb2d1848789700c610da87b10b
Rosario Felix <elijan_11@hotmail.com>
2017-07-17 18:21:15 -0500

    subiendo cambios

--- js/index.js
+++ js/index.js
@@ -1,35 +1,3 @@
-/*
-let lastScrollTop = 0;
-
-const enlace=document.getElementsByClassName('enlace');
-
-
-window.addEventListener("scroll", function(){
-   var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-   if (currentScroll > lastScrollTop){
-     document.getElementById('nav-background').style.display="block";
-     $("#navbar-example").addClass("header-in");
-     document.getElementById("navbar-example").style.opacity = 1;
-
-     enlace[0].style.color="#FFF"
-     enlace[1].style.color="#FFF"
-     enlace[2].style.color="#FFF"
-
-
-   };
-
-   if(currentScroll<=3){
-       document.getElementById("navbar-example").classList.remove("solid");
-       document.getElementById("navbar-example").classList.remove("header-in");
-
-        enlace[0].style.color="#fff"
-        enlace[1].style.color="#fff"
-        enlace[2].style.color="#fff"
-
-     }
-   lastScrollTop = currentScroll;
-}, false);*/
-
 
 let lastScrollTop = 0;
 window.addEventListener('scroll', function(){
@@ -38,7 +6,10 @@ window.addEventListener('scroll', function(){
       document.getElementById("navbar-example").classList.remove("header-in");
   }
   if (currentScroll > 620) {
-      document.getElementById("navbar-example").classList.add("header-in");
+      $("#navbar-example").addClass("header-in");
+  }
+  if(currentScroll > 1193){
+    $(".ico").addClass("animated tada");
   }
   lastScrollTop = currentScroll;
 });
